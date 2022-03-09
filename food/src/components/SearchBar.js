f97b59262b2f64b005c62d64eee5e09d0a5aa7d8
RicoWilliams <ricowilliams@protonmail.com>
2022-03-09 14:13:41 -0700

    searchbar componenet taking props and state being managed from searchscreen parent componenet

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="search" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="search"
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
