167f36c000413cbbb7ba7d20bcc2d38d2e8043b6
RicoWilliams <ricowilliams@protonmail.com>
2022-03-10 13:17:23 -0700

    results object displaying in terminal

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam('id');
+
+  console.log(result);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <View>
-      <Text>Results Screen</Text>
+      <Text>Results Show</Text>
     </View>
   );
 };
